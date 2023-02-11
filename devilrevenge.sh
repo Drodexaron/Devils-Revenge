8c7bbf2c06ff1cec12993fb28ca7bce908e75fcc
Jorge <97186530+Drodexaron@users.noreply.github.com>
2023-02-11 21:39:44 +0630

    Update devilrevenge.sh

--- devilrevenge.sh
+++ devilrevenge.sh
@@ -1,56 +1,45 @@
 #!/bin/bash
 
-# Set the maximum number of requests to 30,000
-max_requests=30000
-
 # Welcome message
-echo -e "\033[1;34mHello, Welcome User!\033[0m"
-echo -e "\033[1;34mThis is in beta version join our telegram channel and wait for update version!\033[0m"
+echo -e "\033[34mHello, Welcome User!\033[0m"
+echo -e "\033[34mThis is in beta version, join our telegram channel and wait for the update version!\033[0m"
 
 # Request range input
-echo -e "\033[1;34mEnter the number of requests you want to send (max $max_requests):\033[0m"
+echo -e "\033[34mEnter the range of requests to be sent (max 100):\033[0m"
 read range
 
 # URL input
-echo -e "\033[1;34mEnter the URL:\033[0m"
+echo -e "\033[34mEnter the URL:\033[0m"
 read url
 
-# Check if range is within the max limit
-if [ $range -gt $max_requests ]; then
-  echo -e "\033[1;31mError: Maximum number of requests exceeded.\033[0m"
-  exit 1
-fi
+# Loop for sending requests
+for i in $(seq 1 $range)
+do
+  # Sending request message
+  echo -e "\033[31m[~]Sending request $i to server!\033[0m \033[32m[$(date +%s%3N) UTC]\033[0m"
 
-# Start sending requests
-start_time=$(date +%s)
-echo -e "\033[1;32m[~] Sending requests to server!\033[0m"
-for i in $(seq 1 $range); do
-  # Send request
+  # Send request using curl
   response=$(curl -s -o /dev/null -w "%{http_code}" $url)
 
-  # Check if server blocked requests
-  if [ $response -ne 200 ]; then
-    echo -e "\033[1;31m[~] Server blocked your requests.\033[0m"
-    echo -e "\033[1;34mWould you like to restart again? (N/Y):\033[0m"
+  # Check if server blocked the request
+  if [ $response -eq 403 ]
+  then
+    echo -e "\033[31m[~]Server blocked your requests.\033[0m"
+
+    # Restart option
+    echo -e "\033[34mWould you like to restart again? (N/Y):\033[0m"
     read restart
-    if [ "$restart" == "Y" ] || [ "$restart" == "y" ]; then
-      # Restart sending requests
-      continue
+
+    if [ "$restart" == "Y" ] || [ "$restart" == "y" ]
+    then
+      i=0
     else
-      echo -e "\033[1;34mSee you soon!\033[0m"
+      echo -e "\033[34mSee you soon!\033[0m"
       exit 0
     fi
   fi
-  sleep 1
-  echo -e "\033[1;32m[~] Sending requests to server! $(date -u)\033[0m"
 done
 
-# Calculate total time
-end_time=$(date +%s)
-total_time=$((end_time-start_time))
+# End message
+echo -e "\033[34mFinished sending requests to server.\033[0m"
 
-# Display results
-echo -e "\033[1;32mFinished sending $range requests.\033[0m"
-echo -e "\033[1;32mStarted at: $(date -d @$start_time)\033[0m"
-echo -e "\033[1;32mEnded at: $(date -d @$end_time)\033[0m"
-echo -e "\033[1;32mTotal time: $total_time seconds\033[0m"
